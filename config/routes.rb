e81feee3592473e3724984536d3a7d999740002f
Charles Darwin Pobre <cpobre.peg@gmail.com>
2015-03-12 00:00:30 +0800

    updated UI

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get 'edit_profile' => 'applicants#edit'
   patch 'update_profile' => 'applicants#update'
+  get 'draft' => 'applicants#draft_page'
 
   root to: 'welcome#index'
 end
