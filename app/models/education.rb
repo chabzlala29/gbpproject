80e195fc082486a7c07b971bbd7e0f576e6d29e3
jabss118 <javeelineac@gmail.com>
2015-03-12 18:16:41 +0800

    UI changes

--- app/models/education.rb
+++ app/models/education.rb
@@ -1,2 +1,13 @@
 class Education < ActiveRecord::Base
+	LEVEL_LISTS = ['','Elementary Graduate ', 
+'High School Graduate ', 
+'High School Undergraduate ', 
+'College Graduate ', 
+'College Undergraduate ', 
+'Vocational ', 
+'Associate degree ', 
+'Bachelors degree ', 
+'Masters degree ', 
+'Professional degree ', 
+'Doctorate ']
 end
--- app/models/job_preference.rb
+++ app/models/job_preference.rb
@@ -1,7 +1,7 @@
 class JobPreference < ActiveRecord::Base
   belongs_to :applicant
 
-  SOURCE_LISTS = ['Social Media', 'Ads', 'Friend']
+  SOURCE_LISTS = ['Select', 'Social Media', 'Ads', 'Friend','Newspaper Ad ', 'Current Employee ']
 
   attr_accessor :source_sub
 end
