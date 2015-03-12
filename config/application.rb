056974babca2c733326039991c478dc543c583e1
Charles Darwin Pobre <cpobre.peg@gmail.com>
2015-03-12 21:43:44 +0800

    moved settings to yml

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,11 @@ Bundler.require(*Rails.groups)
 
 module Gbpproject
   class Application < Rails::Application
+
+    def self.settings
+      YAML.load_file('config/settings.yml')
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
